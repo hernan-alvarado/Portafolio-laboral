1c5d31e6a3ffab24b77c8ecc3cda1721d3e52d77
Hernán Alvarado <hernnalvarado@gmail.com>
2025-03-20 18:38:41 -0300

    se mejoran secciones mis proyectos, tecnologias, certificaciones y footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,41 @@
 
-const swiper = new Swiper(".swiper", {
-	autoplay: {
-		delay: 4000,
-	  },
-	effect: 'fade',
-	fadeEffect: {
-	  crossFade: true
-	},
+const proyectosSwiper = new Swiper(".proyectosSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        }
+    }
 });
 
 
-
-
+const certificacionesSwiper = new Swiper(".certificacionesSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        }
+    }
+});
