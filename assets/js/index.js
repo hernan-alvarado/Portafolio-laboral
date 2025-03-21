9ede7e52d2d54b9b09034c8feed93472acbd2f90
Hernán Alvarado <hernnalvarado@gmail.com>
2025-03-21 14:02:11 -0300

    cambios en el diseño

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,35 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    const text = "¡Hola! Soy Hernán Alvarado.//Desarrollador FullStack JavaScript//Especializado en aplicaciones modernas//+2 años de experiencia"; 
+    const element = document.getElementById('typewriter');
+    const phrases = text.split('//');
+    let i = 0;
+    let j = 0;
+    let currentPhrase = [];
+    
+    function typeWriter() {
+        if (i < phrases.length) {
+            if (j < phrases[i].length) {
+                currentPhrase.push(phrases[i][j]);
+                element.innerHTML = currentPhrase.join('') + '<span class="cursor">|</span>';
+                j++;
+                setTimeout(typeWriter, 50);
+            } else {
+                currentPhrase = [];
+                element.innerHTML += '<br><br>';
+                i++;
+                j = 0;
+                setTimeout(typeWriter, 1000);
+            }
+        }
+    }
+    
+    typeWriter();
+});
+
 const proyectosSwiper = new Swiper(".proyectosSwiper", {
-    slidesPerView: 1,
+    slidesPerView: 'auto',
+    centeredSlides: true,
     spaceBetween: 30,
     loop: true,
     navigation: {
@@ -9,33 +38,32 @@ const proyectosSwiper = new Swiper(".proyectosSwiper", {
     },
     breakpoints: {
         768: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
-        992: {
-            slidesPerView: 1,
-            spaceBetween: 30
+            spaceBetween: 40
         }
     }
 });
 
-
 const certificacionesSwiper = new Swiper(".certificacionesSwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 20,
     loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20
+        640: {
+            spaceBetween: 15,
+            slidesPerView: 1
         },
-        1200: {
-            slidesPerView: 2,
-            spaceBetween: 30
+        768: {
+            spaceBetween: 20,
+            slidesPerView: 1
+        },
+        1024: {
+            spaceBetween: 25,
+            slidesPerView: 1
         }
     }
 });
